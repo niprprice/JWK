10795d9bf1332eea8a7128206632e92f115e226d
HHH <hhh@HHHs-MBP.modem>
2020-10-26 22:54:59 +1100

    Test1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log("Server started"));
 
